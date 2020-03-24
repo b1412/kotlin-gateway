b3872b7fbed41adf46339975a1f2e411f5bc8125
leon.zhou <leon.zhou@vendhq.com>
2020-03-24 18:39:45 +1300

    add payment route

--- src/main/kotlin/com/github/b1412/gateway/AdditionalRoutes.kt
+++ src/main/kotlin/com/github/b1412/gateway/AdditionalRoutes.kt
@@ -2,7 +2,6 @@ package com.github.b1412.gateway
 
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
-import org.springframework.cloud.gateway.route.builder.filters
 import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -15,6 +14,10 @@ class AdditionalRoutes {
             route(id = "api") {
                 path("/v1/**")
                 uri("lb://api/")
+            }
+            route(id = "payment") {
+                path("/payment/**")
+                uri("lb://payment/")
             }
         }
     }
